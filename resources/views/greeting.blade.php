ad4ff236bcbe76ebd8bc99a2d5b8048fab07dcd6
Faizan <faizan121234@gmail.com>
2022-04-06 23:46:52 -0700

    adding the gitignore back

--- resources/views/greeting.blade.php
+++ resources/views/greeting.blade.php
@@ -1,10 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
 
-</head>
-<body>
-    <h1>hello, {{$name}} </h1>
+
+    <!-- Header Block -->
+    <x-header/>
+    
+    <!-- content block -->
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+            <div class="col">
+                <a href="https://placeholder.com"><img src="https://via.placeholder.com/185x275"></a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Footer Block -->
+    <x-footer/>
+
+
+    <!-- <h1>hello, {{$name}} </h1>
     
     <div>
         <h1> {{ $movie_title }} Watched on: <time></time></h1>
@@ -12,6 +57,4 @@
             <h3> {{ $name}} Review</h3>
             The Matrix was the the best movie I have ever seen!
         </p>
-    </div>
-</body>
-</html>
+    </div> -->
