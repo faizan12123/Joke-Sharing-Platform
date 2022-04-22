a523c7ef9b73e1cc75fa846ace96b50aac6c655d
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-04-22 16:16:47 -0700

    added stylesheet, navigation bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trenc | Find all your favorite memes in one place! </title>
     <link rel="icon" type="image/x-icon" href="logo.png">
+    <link rel="stylesheet" type="text/css" href={{ asset('css/style.css') }}>
 </head>
 <body>
-    
+    <x-navigationBar/>
 </body>
 </html>
