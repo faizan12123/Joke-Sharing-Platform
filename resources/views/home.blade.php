0adceae624642d2678d4bddc7903e73ad68d08bc
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-04-22 16:58:05 -0700

    added sidebar and main feed to home page layout #1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,14 @@
     <title>Trenc | Find all your favorite memes in one place! </title>
     <link rel="icon" type="image/x-icon" href="logo.png">
     <link rel="stylesheet" type="text/css" href={{ asset('css/style.css') }}>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/35e6bdb001.js" crossorigin="anonymous"></script>
 </head>
 <body>
+
     <x-navigationBar/>
+    <x-leftSideBar/>
+    <x-homeFeed/>
+    
 </body>
 </html>
