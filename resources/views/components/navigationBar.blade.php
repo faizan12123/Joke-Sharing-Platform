7d963a3f6545c54a9ff485d93d97f3d5166e5638
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-04-22 17:02:35 -0700

    removed font awesome icon; bugs #1

---
+++ resources/views/components/navigationBar.blade.php
@@ -0,0 +1,9 @@
+<nav>
+    <div class="nav-left">
+        <img class="nav-logo" src="logo.png"/>
+        <p> Trunc </p>
+    </div>
+    <div class="nav-right">
+        <p> Log in</p>
+    </div>
+</nav>
