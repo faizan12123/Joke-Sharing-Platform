ac6fe2de5b3c593c4c108f32a658f1c8163b4d5d
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-05-04 18:44:12 -0700

    added button markup to left sidebar

--- resources/views/components/leftSideBar.blade.php
+++ resources/views/components/leftSideBar.blade.php
@@ -1,6 +1,10 @@
 
 <link rel="stylesheet" href={{ asset('css/bootstrap.min.css') }}>
 <section class="leftSideBar-container">
+
+    <div class="leftSideBar-btn">
+        <a href="/createPost" title="Post Joke"> NEW </a>
+    </div>
     <h2> Categories </h2>
     <ul>
         <li><a href="/tech">Tech</a></li>
