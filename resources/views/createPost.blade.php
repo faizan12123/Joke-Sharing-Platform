03ed6076fd00d303ad396d9d48dbc186927adbb3
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-05-04 19:35:44 -0700

    #10 removed bootstrap styling

--- resources/views/createPost.blade.php
+++ resources/views/createPost.blade.php
@@ -20,16 +20,16 @@
         <div class="createPost-container">
             <form method="POST" action="/createPost">
                 @csrf
-                <div class="form-group">
+                <div class="">
                     <input type="text" placeholder="Name" class="form-control" id="name" name="name">
                 </div>
 
-                <div class="form-group">
+                <div class="">
                     <input type="text" placeholder="Genre"  class="form-control" id="genre" name="genre">
                 </div>
                 
 
-                <div class="form-group">
+                <div class="">
                     <input type="text" placeholder="Joke"  class="form-control" id="joke" name="joke">
                 </div>
  
