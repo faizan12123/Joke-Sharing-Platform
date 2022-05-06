26f3a0ee986e23b3660ffb4c7e6c4f3ec2cbed5f
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-05-05 21:06:21 -0700

    #11 debugging

--- resources/views/createRating.blade.php
+++ resources/views/createRating.blade.php
@@ -22,15 +22,15 @@
             <form class="rate" method="POST" action="/createRating">
                 @csrf
                 <input type="text" placeholder="Name" class="" id="name" name="name"><br/>
-                <input type="radio" id="star5" name="rate" value="5" />
+                <input type="radio" id="star5" name="rating" value="5" />
                 <label for="star5" title="text">5 stars</label>
-                <input type="radio" id="star4" name="rate" value="4" />
+                <input type="radio" id="star4" name="rating" value="4" />
                 <label for="star4" title="text">4 stars</label>
-                <input type="radio" id="star3" name="rate" value="3" />
+                <input type="radio" id="star3" name="rating" value="3" />
                 <label for="star3" title="text">3 stars</label>
-                <input type="radio" id="star2" name="rate" value="2" />
+                <input type="radio" id="star2" name="rating" value="2" />
                 <label for="star2" title="text">2 stars</label>
-                <input type="radio" id="star1" name="rate" value="1" />
+                <input type="radio" id="star1" name="rating" value="1" />
                 <label for="star1" title="text">1 star</label>
                 <br/><br/><br/>
                 <button type="submit" style="" class="">Submit</button>
